ea46c3e62ea015a9e203bd5c79cfbeedf1edb75a
RuochenLiu <rl2841@columbia.edu>
2017-03-22 22:02:56 -0400

    Revised Train+CV & PHASH

--- lib/Train+CV.R
+++ lib/Train+CV.R
@@ -1,11 +1,10 @@
-#GBM
-
 library("caret")
 library("gbm")
 library("randomForest")
 library("plyr")
 library("xgboost")
 library("fastAdaboost")
+library("deepboost")
 
 X <- read.csv("../data/sift_features.csv")
 X <- t(X)
@@ -13,8 +12,8 @@ X <- t(X)
 labels <- read.csv("../data/labels.csv")
 labels <- labels[,1]
 #dat_train<-X
-#dat_train<-dat
-dat_train<-cbind(X,H)
+dat_train<-H
+#dat_train<-cbind(P,H)
 label_train <- labels
 
 
@@ -42,49 +41,55 @@ label_train <- labels
   
   testing <- data.all[-inTrain, ]
   
-  # GBM
+  ### GBM
   gbmGrid <- expand.grid(interaction.depth = (1:5) * 2,n.trees = (1:10)*25,shrinkage = .1,
                          n.minobsinnode = 10)
   
   gbmfit <- train(Label~., data = training,
                   method = "gbm", trControl = control, verbose = FALSE,
                   bag.fraction = 0.5, tuneGrid = gbmGrid
-  ) #parameter tuning
+  )
   
   err.gbm <- sum(predict(gbmfit, testing) != testing$Label)/nrow(testing)
   
-  # Random Forest
+  ### Random Forest
   rfGrid <- expand.grid(mtry = 2^(5:10) )
   
   rffit <- train(Label~., data = training,
                  method = "rf", trControl = control, tuneGrid = rfGrid
-  ) #parameter tuning
+  ) 
   
   err.rf <- sum(predict(rffit, testing) != testing$Label)/nrow(testing)
   
   
-  # SVM Linear Kernel
+  ### SVM Linear
   svmGrid.linear <- expand.grid(C= 2^c(0:5))
   svmfit.linear <- train(Label~., data = training,
                          method = "svmLinear", trControl = control, tuneGrid = svmGrid.linear, preProc = c("center","scale")
-  ) #parameter tuning
+  )
   
   err.svm.linear <- sum(predict(svmfit.linear, testing) != testing$Label)/nrow(testing)
   
-  # SVM RBF
+  ### SVM RBF
   svmGrid <- expand.grid(sigma= 2^c(-25, -20, -15,-10, -5, 0), C= 2^c(0:5))
   svmfit <- train(Label~., data = training,
                   method = "svmRadial", trControl = control, tuneGrid = svmGrid, preProc = c("center","scale")
-  ) #parameter tuning
+  )
   
   err.svm <- sum(predict(svmfit, testing) != testing$Label)/nrow(testing)
   
-  # xgBoost
-  xgbfit <- train(Label~., data = training[1:100,],
+  ### xgBoost
+  xgbfit <- train(Label~., data = training,
                   method = "xgbLinear", trControl = control
-  ) #parameter tuning
+  )
   
   err.xgb <- sum(predict(xgbfit, testing) != testing$Label)/nrow(testing)
+  
+  ### DeepBoost
+  dbfit <- train(Label~., data = training,
+                  method = "deepboost", trControl = control, verbose = FALSE
+  )
+  err.db <- sum(predict(dbfit, testing) != testing$Label)/nrow(testing)
   
 #}
 
