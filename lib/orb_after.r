a9538a9859c0f710aa6e277b00323aa335019ff7
Xiaowo Sun <prayerest@users.noreply.github.com>
2017-03-23 15:24:38 -0400

    orb feature

--- lib/orb_after.r
+++ lib/orb_after.r
@@ -2,30 +2,29 @@ library(data.table)
 library(dplyr)
 library(gbm)
 
-#feature.csv derives from .py file
 orb_features <- fread('../data/orb_feature.csv',header = FALSE)
 
 orb_features <- as.data.frame(orb_features)
 colnames(orb_features)[33] <- c("Image_index")
 dim(orb_features)
-#orb_features[,33]
+
 set.seed(333)
 orb_features_cluster <- kmeans(orb_features[,c(1:32)], centers = 500)
 length(orb_features_cluster$size)
 length(orb_features_cluster$cluster)
-# number of training images
+
 num_of_image <- orb_features[dim(orb_features)[1],dim(orb_features)[2]]
 
 index <- rep(1:length(orb_features_cluster$cluster),1)
 orb_cluster_500 <- matrix(NA, nrow = 500, ncol = num_of_image)
 
-#get new probability matrix 
+#new prob matrix 
 for (i in 1:num_of_image){
   if (length(index[orb_features$Image_index == i]) == 0)
     orb_cluster_500[,i] = 0
   else{
     kp_list <- index[orb_features$Image_index == i]
-    #500 clusters
+    # clusters# 500
     for (k in 1 : 500){
       cluster_list <- index[orb_features_cluster$cluster == k]
       total_kp <- length(kp_list)
--- lib/orb_extract.py
+++ lib/orb_extract.py
@@ -3,9 +3,9 @@ import csv
 import cv2
 from os import listdir
 
-# dir of training images
+# dir for training images
 path1 = '/Users/Prayerest_/Github/spr2017-proj3-group7-master/data/raw_images/'
-# dir of burred images
+# dir for burred images
 path2 = '/Users/Prayerest_/Github/spr2017-proj3-group7-master/data/burred_images/'
 
 
@@ -18,16 +18,14 @@ for i in range(len(image_lists)-1):
     image_dir_list.append(path1+image_lists[i+1])
     burred_dir_list.append(path2+image_lists[i+1])
 
-#Filter
+
 # get burred images
 for i in range(len(image_dir_list)):
     print(i)
     img = cv2.imread(image_dir_list[i])
     median = cv2.medianBlur(img,7)
-    #blur = cv2.bilateralFilter(img,15,75,75)
     cv2.imwrite(burred_dir_list[i],median)
     
-
 
 # ORB
 num = 0
@@ -37,9 +35,9 @@ for i in range(len(burred_dir_list)):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
     orb = cv2.ORB_create(nfeatures=50)
 
-    # find the keypoints with ORB
+    # find the keypoints
     kp = orb.detect(gray,None)
-    # compute the descriptors with ORB
+    # compute the descriptors
     kp, des = orb.compute(gray, kp)
     
     label = np.array([i+1])
